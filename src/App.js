64d273954b3313565937763b3e93dc40748bdc4f
saket96 <31483649+saket96@users.noreply.github.com>
2021-06-01 08:50:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>check box no 1</p>
+      <input type="checkbox" />
+      <p>check box no 1</p>
+      <input type="checkbox" />
+      <p>check box no 1</p>
+      <input type="checkbox" />
+      <p>check box no 1</p>
+    </div>
+  );
+}
+export default App;
